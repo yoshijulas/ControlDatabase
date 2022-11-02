4179899abd437aa7b87c61945992d4e2aff83964
yoshijulas <yoshijulas@live.com>
2022-11-01 21:43:55 -0600

    Database Control Publish

---
+++ main.js
@@ -0,0 +1,169 @@
+/* eslint-disable no-undef */
+$(document).ready(() => {
+  $('#anterior').click(() => {
+    const anterior = true;
+    const matricula = $('#matricula').val();
+
+    $.post(
+      'query_pdo.php',
+      {
+        anterior,
+        matricula,
+      },
+      (response) => {
+        if (response === '0') {
+          $('#result').html('No hay registros anteriores');
+        } else {
+          const myArray = JSON.parse(response);
+
+          $('#matricula').val(myArray[0]);
+          $('#nombre').val(myArray[1]);
+          $('#telefono').val(myArray[2]);
+          $('#edad').val(myArray[3]);
+          $('#result').html('Registro anterior');
+        }
+      },
+    );
+
+    return false;
+  });
+
+  $('#crear').click(() => {
+    const crear = true;
+    const matricula = $('#matricula').val();
+    const nombre = $('#nombre').val();
+    const telefono = $('#telefono').val();
+    const edad = $('#edad').val();
+
+    if (matricula === '' || nombre === '' || telefono === '' || edad === '') {
+      $('#result').html('Todos los campos son obligatorios');
+    } else {
+      $.post(
+        'query_pdo.php',
+        {
+          crear,
+          matricula,
+          nombre,
+          telefono,
+          edad,
+        },
+        (response) => {
+          $('#result').html(response);
+        },
+      );
+    }
+    return false;
+  });
+
+  $('#buscar').click(() => {
+    const buscar = true;
+    const matricula = $('#matricula').val();
+
+    if (matricula === '') {
+      $('#result').html('Matricula obligatoria');
+    } else {
+      $.post(
+        'query_pdo.php',
+        {
+          buscar,
+          matricula,
+        },
+        (response) => {
+          const myArray = JSON.parse(response);
+
+          if (myArray[0] === '') {
+            $('#result').html('No hay registros');
+          } else {
+            $('#matricula').val(myArray[0]);
+            $('#nombre').val(myArray[1]);
+            $('#telefono').val(myArray[2]);
+            $('#edad').val(myArray[3]);
+
+            $('#result').html('Registro encontrado');
+          }
+        },
+      );
+    }
+    return false;
+  });
+
+  $('#eliminar').click(() => {
+    const eliminar = true;
+
+    const matricula = $('#matricula').val();
+
+    if (matricula === '') {
+      $('#result').html('Todos los campos son obligatorios');
+    } else {
+      $.post(
+        'query_pdo.php',
+        {
+          eliminar,
+          matricula,
+        },
+        (response) => {
+          $('#result').html(response);
+        },
+      );
+    }
+    return false;
+  });
+
+  $('#actualizar').click(() => {
+    const actualizar = true;
+    const matricula = $('#matricula').val();
+    const nombre = $('#nombre').val();
+    const telefono = $('#telefono').val();
+    const edad = $('#edad').val();
+
+    if (matricula === '' || nombre === '' || telefono === '' || edad === '') {
+      $('#result').html('Todos los campos son obligatorios');
+    } else {
+      $.post(
+        'query_pdo.php',
+        {
+          actualizar,
+          matricula,
+          nombre,
+          telefono,
+          edad,
+        },
+        (response) => {
+          $('#result').html(response);
+        },
+      );
+    }
+    return false;
+  });
+
+  $('#siguiente').click(() => {
+    const siguiente = true;
+    const matricula = $('#matricula').val();
+
+    $.post(
+      'query_pdo.php',
+      {
+        siguiente,
+        matricula,
+      },
+      (response) => {
+        if (response === '0') {
+          $('#result').html('No hay registros siguientes');
+        } else {
+          const myArray = JSON.parse(response);
+
+          $('#matricula').val(myArray[0]);
+          $('#nombre').val(myArray[1]);
+          $('#telefono').val(myArray[2]);
+          $('#edad').val(myArray[3]);
+
+          $('#result').html('Registro siguiente');
+        }
+      },
+    );
+
+    return false;
+  });
+});
+
+// Josue Aburto Perez K021
